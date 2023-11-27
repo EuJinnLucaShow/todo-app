0344efcb20fe0bac54ba5bae69c0ab478db7b97d
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-11-27 10:09:28 +0200

    integrate pagination using the react-paginate

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,24 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import TodoItem from './TodoItem';
 import { selectTodo } from 'redux/selectors';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTodos } from 'redux/operations';
+import ReactPaginate from 'react-paginate';
+
+const PAGE_SIZE = 5;
 
 const TodoList = () => {
   const todos = useSelector(selectTodo);
-  const reversedTodos = [...todos].reverse();
+  const [currentPage, setCurrentPage] = useState(0);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchTodos());
   }, [dispatch]);
 
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  const paginatedTodos = todos.slice(
+    currentPage * PAGE_SIZE,
+    (currentPage + 1) * PAGE_SIZE,
+  );
+
   return (
-    <ul className="tasks">
-      {reversedTodos.map(todo => (
-        <TodoItem key={todo.id} todo={todo} />
-      ))}
-    </ul>
+    <div>
+      <ul className="tasks">
+        {paginatedTodos.map(todo => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+      <ReactPaginate
+        previousLabel={'Previous'}
+        nextLabel={'Next'}
+        breakLabel={'...'}
+        pageCount={Math.ceil(todos.length / PAGE_SIZE)}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageChange}
+        containerClassName={'pagination'}
+        activeClassName={'active'}
+      />
+    </div>
   );
 };
 
