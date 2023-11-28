3fc3c6270fa8b0b92aea4bd9e1e450a7b7f920c8
Yevhenii Lukashov <115802889+EuJinnLucaShow@users.noreply.github.com>
2023-11-28 17:57:26 +0200

    The ability to edit the title has been implemented

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+// import React from 'react';
+// import { useDispatch } from 'react-redux';
+// import { deleteTodo, editTodo } from 'redux/operations';
+
+// const TodoItem = ({ todo }) => {
+//   const dispatch = useDispatch();
+
+//   const deleteTask = id => {
+//     dispatch(deleteTodo(id));
+//   };
+
+//   const toggleTaskStatus = id => {
+//     const updatedTask = { id, completed: !todo.completed };
+//     dispatch(editTodo(updatedTask));
+//   };
+
+//   if (!todo) {
+//     return null;
+//   }
+
+//   return (
+//     <li className="task">
+//       <span className={todo.completed ? 'checked' : 'none'}>{todo.title}</span>
+//       <div className="btn-wrapper">
+//         <button className="btn-done" onClick={() => toggleTaskStatus(todo.id)}>
+//           Done
+//         </button>
+//         <button className="btn-delete" onClick={() => deleteTask(todo.id)}>
+//           Delete
+//         </button>
+//       </div>
+//     </li>
+//   );
+// };
+
+// export default TodoItem;
+
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteTodo, editTodo } from 'redux/operations';
 
 const TodoItem = ({ todo }) => {
   const dispatch = useDispatch();
+  const [editable, setEditable] = useState(false);
+  const [editedTitle, setEditedTitle] = useState(todo.title);
 
   const deleteTask = id => {
     dispatch(deleteTodo(id));
@@ -14,20 +53,70 @@ const TodoItem = ({ todo }) => {
     dispatch(editTodo(updatedTask));
   };
 
+  const handleEdit = () => {
+    setEditable(true);
+  };
+
+  const handleSave = () => {
+    const updatedTask = { ...todo, title: editedTitle };
+    dispatch(editTodo(updatedTask));
+    setEditable(false);
+  };
+
+  const handleCancel = () => {
+    setEditedTitle(todo.title);
+    setEditable(false);
+  };
+
+  const handleInputChange = event => {
+    setEditedTitle(event.target.value);
+  };
+
   if (!todo) {
     return null;
   }
 
   return (
     <li className="task">
-      <span className={todo.completed ? 'checked' : 'none'}>{todo.title}</span>
+      {editable ? (
+        <input
+          className="input-edit"
+          type="text"
+          value={editedTitle}
+          onChange={handleInputChange}
+          autoFocus
+        />
+      ) : (
+        <span className={todo.completed ? 'checked' : 'none'}>
+          {todo.title}
+        </span>
+      )}
       <div className="btn-wrapper">
-        <button className="btn-done" onClick={() => toggleTaskStatus(todo.id)}>
-          Done
-        </button>
-        <button className="btn-delete" onClick={() => deleteTask(todo.id)}>
-          Delete
-        </button>
+        {editable ? (
+          <>
+            <button className="btn-save" onClick={handleSave}>
+              Save
+            </button>
+            <button className="btn-cancel" onClick={handleCancel}>
+              Cancel
+            </button>
+          </>
+        ) : (
+          <>
+            <button className="btn-edit" onClick={handleEdit}>
+              Edit
+            </button>
+            <button
+              className="btn-done"
+              onClick={() => toggleTaskStatus(todo.id)}
+            >
+              Done
+            </button>
+            <button className="btn-delete" onClick={() => deleteTask(todo.id)}>
+              Delete
+            </button>
+          </>
+        )}
       </div>
     </li>
   );
